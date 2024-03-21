32aa18b95e95cb4a48a9241a8091997694f4ff96
Atila Silva <atila__7@hotmail.com>
2024-03-21 06:49:47 -0300

    feat: Project created

---
+++ src/main/java/com/example/technicalcase/TechnicalCaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.technicalcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalCaseApplication.class, args);
+	}
+
+}
