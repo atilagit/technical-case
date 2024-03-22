529f5e6a060319f53ef302cc6ffaa4cf59578ffd
Atila Silva <atila__7@hotmail.com>
2024-03-22 01:05:40 -0300

    fix: Serializable implementation in the User class

--- src/main/java/com/example/technicalcase/entities/User.java
+++ src/main/java/com/example/technicalcase/entities/User.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
@@ -17,7 +18,8 @@ import java.util.Objects;
 @Setter
 @Entity
 @Table(name = "TB_USER")
-public class User {
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
