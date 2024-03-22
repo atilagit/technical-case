1d6516215c9e9cdd14863c3da1d4add69c210c31
Atila Silva <atila__7@hotmail.com>
2024-03-22 02:22:18 -0300

    feat: Implementation max size of username

--- src/main/java/com/example/technicalcase/controller/data/requests/UserRequest.java
+++ src/main/java/com/example/technicalcase/controller/data/requests/UserRequest.java
@@ -1,15 +1,22 @@
 package com.example.technicalcase.controller.data.requests;
 
 import com.example.technicalcase.enumerators.Role;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 
 public record UserRequest(
-        @NotBlank(message = "Campo obrigatório") String name,
-        @Pattern(regexp = "^[a-z]+$", message = "O username deve conter apenas letras minúsculas") String username,
-        @Email(message = "Digite um email válido") String email,
-        @NotBlank(message = "Campo obrigatório") String password,
-        @NotNull(message = "Campo obrigatório") Role role
+        @NotBlank(message = "Campo obrigatório")
+        String name,
+
+        @Pattern(regexp = "^[a-z]+$", message = "O username deve conter apenas letras minúsculas")
+        @Size(max = 20, message = "Ultrapassou o limite máximo de caractes")
+        String username,
+
+        @Email(message = "Digite um email válido")
+        String email,
+
+        @NotBlank(message = "Campo obrigatório")
+        String password,
+
+        @NotNull(message = "Campo obrigatório")
+        Role role
 ) {}
