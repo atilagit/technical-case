2eb654ef94f4dba160a130495284ef52df50e438
Atila Silva <atila__7@hotmail.com>
2024-03-23 10:13:43 -0300

    feat: Feature to allow only instructors to author a course

--- src/main/java/com/example/technicalcase/services/validation/InsertCourseValidator.java
+++ src/main/java/com/example/technicalcase/services/validation/InsertCourseValidator.java
@@ -2,15 +2,20 @@ package com.example.technicalcase.services.validation;
 
 import com.example.technicalcase.controller.data.requests.InsertCourseRequest;
 import com.example.technicalcase.controller.exceptions.FieldMessage;
-import com.example.technicalcase.entities.Course;
+import com.example.technicalcase.entities.User;
 import com.example.technicalcase.repositories.CourseRepository;
+import com.example.technicalcase.repositories.UserRepository;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public record InsertCourseValidator(CourseRepository repository)
+import static com.example.technicalcase.enumerators.Role.INSTRUCTOR;
+import static java.lang.Boolean.FALSE;
+import static java.util.Objects.isNull;
+
+public record InsertCourseValidator(CourseRepository courseRepository, UserRepository userRepository)
         implements ConstraintValidator<InsertCourseValidation, InsertCourseRequest> {
 
     @Override
@@ -21,9 +26,16 @@ public record InsertCourseValidator(CourseRepository repository)
     public boolean isValid(InsertCourseRequest request, ConstraintValidatorContext constraintValidatorContext) {
         List<FieldMessage> fieldMessages = new ArrayList<>();
 
-        Course course = repository.findByCode(request.code());
+        var course = courseRepository.findByCode(request.code());
         if(course != null) {
             fieldMessages.add(new FieldMessage("code", request.code() + " already exists."));
+        }
+
+        var user = userRepository.findByUsername(request.instructorUsername());
+        if(isNull(user)) {
+            fieldMessages.add(new FieldMessage("instructorUsername", request.instructorUsername() + " does not exist in the database"));
+        } else if(FALSE.equals(isAnInstructor(user))) {
+            fieldMessages.add(new FieldMessage("instructorUsername", request.instructorUsername() + " is not an instructor"));
         }
 
         for (FieldMessage e : fieldMessages) {
@@ -33,4 +45,9 @@ public record InsertCourseValidator(CourseRepository repository)
         }
         return fieldMessages.isEmpty();
     }
+
+    private boolean isAnInstructor(User user) {
+        return user.getRole().equals(INSTRUCTOR);
+    }
+
 }
